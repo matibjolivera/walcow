536b95383d5ed49c8df07062d54856340ae82f03
Matías Olivera <matibjolivera@gmail.com>
2021-06-08 19:48:40 -0300

    tokens

--- index.js
+++ index.js
@@ -2,13 +2,16 @@ const express = require("express");
 const mongoose = require("mongoose");
 const Cors = require("cors");
 const bodyParser = require("body-parser");
+
 require("dotenv").config();
-const usersRouter = require("./routes/users");
 
 // App config
 const app = express();
 const port = process.env.PORT || 4000;
 const { DATABASE } = require("./config");
+
+const usersRouter = require("./routes/users");
+const tokensRouter = require("./routes/tokens");
 
 //Middlewares
 app.use(express.json());
@@ -17,13 +20,11 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use("/api/users", usersRouter);
+app.use("/api/tokens", tokensRouter);
 
 mongoose
   .connect(DATABASE, { useNewUrlParser: true })
   .then((r) => console.log("Conexión OK"));
-
-//Api end points
-app.get("/", (req, res) => res.status(200).send("Hello Cow Boys"));
 
 //Listeners
 app.listen(port, () => console.log(`listening on port ${port}`));
---
+++ routes/tokens.js
@@ -0,0 +1,37 @@
+const express = require("express");
+const router = express.Router();
+const Token = require("../models/Token");
+
+router.get("/", async function (req, res) {
+    res.send(await Token.find());
+});
+
+router.post("/", async function (req, res) {
+    let token = new Token({
+        code: req.body.code,
+        name: req.body.name
+    });
+
+    await token.save();
+    res.send(token);
+});
+
+router.patch("/:code", async function (req, res) {
+    if (!req.params.code) {
+        res.status(400);
+        res.send('"code" must be sent');
+    }
+
+    let token = await Token.findOne({code: req.params.code});
+
+    for (let k in req.body) {
+        let v = req.body[k];
+        if (Token.schema.obj.hasOwnProperty(k)) {
+            token[k] = v;
+        }
+    }
+
+    await token.save();
+    res.send(token);
+});
+module.exports = router;

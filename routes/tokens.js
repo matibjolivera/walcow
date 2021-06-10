e6b3f012a3cc861e28d3bd8f7e9cbaee0ccd5cee
Matías Olivera <matibjolivera@gmail.com>
2021-06-09 21:40:57 -0300

    cotización en tiempo real de un token

--- routes/tokens.js
+++ routes/tokens.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const router = express.Router();
 const Token = require("../models/Token");
+const fetch = require("node-fetch");
+
+const API = 'https://api.coingecko.com/api/v3/';
 
 router.get("/", async function (req, res) {
     res.send(await Token.find());
@@ -34,4 +37,22 @@ router.patch("/:code", async function (req, res) {
     await token.save();
     res.send(token);
 });
+
+router.get('/price/:id', async (req, res) => {
+    if (!req.params.id) {
+        res.status(400);
+        res.send('"id" must be sent');
+    }
+
+    let response = await fetch(`${API}coins/${req.params.id}`)
+    let json = await response.json();
+
+    res.send({
+        id: json.id,
+        symbol: json.symbol,
+        name: json.name,
+        price: json.market_data.current_price.usd
+    });
+})
+
 module.exports = router;

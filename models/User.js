a6171d3dcc7d14a3b93a82413fa6e96a0714a200
Matías Olivera <matibjolivera@gmail.com>
2021-06-16 20:40:52 -0300

    hook after create user that create a wallet for each token

--- models/User.js
+++ models/User.js
@@ -1,20 +1,40 @@
-const {model, Schema} = require("mongoose");
+const { model, Schema } = require("mongoose");
 const crypto = require("crypto");
-module.exports = model(
-    "User",
-    new Schema({
-        username: {type: String, required: true},
-        password: {type: String, required: true},
-        email: {type: String, required: true},
-        firstname: {type: String, required: true},
-        lastname: {type: String, required: true},
-        token: {
-            type: String,
-            required: true,
-            default: crypto.randomBytes(20).toString("hex"),
-        },
-        wallet: [{type: Schema.Types.ObjectId, ref: "Wallet"}],
-        fiat: {type: Number, default: Number(0)},
-        created_at: {type: Date, default: Date.now},
+
+const Wallet = require("../models/Wallet");
+const Token = require("../models/Token");
+
+const ObjectId = require("mongoose").Types.ObjectId
+
+const schema = new Schema({
+    username: {type: String, required: true},
+    password: {type: String, required: true},
+    email: {type: String, required: true},
+    firstname: {type: String, required: true},
+    lastname: {type: String, required: true},
+    token: {
+        type: String,
+        required: true,
+        default: crypto.randomBytes(20).toString("hex"),
+    },
+    wallet: [{type: Schema.Types.ObjectId, ref: "Wallet"}],
+    fiat: {type: Number, default: Number(0)},
+    created_at: {type: Date, default: Date.now},
+});
+
+schema.post('save', function (u) {
+    Token.find().map(async t => {
+        await new Wallet({
+            user: new ObjectId(u._id),
+            token: new ObjectId(t._id),
+            quantity: 0
+        }).save()
     })
+});
+
+const User = model(
+    "User",
+    schema
 );
+
+module.exports(User)

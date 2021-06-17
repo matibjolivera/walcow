680080f67d7ff54c4ea128396f57c310ae8d0e2c
Matías Olivera <matibjolivera@gmail.com>
2021-06-16 22:28:29 -0300

    transaction strategy

--- routes/wallets.js
+++ routes/wallets.js
@@ -83,41 +83,48 @@ router.post("/", async function (req, res) {
 });
 
 router.post('/sell', async (req, res) => {
-    if (!req.body.token || !req.body.user || !req.body.price || !req.body.quantity) {
-        res.status(400);
-        res.send(response(false, '"token" & "user" & "price" & "quantity" must be sent'));
-    }
-
-    let user = await User.findOne({_id: req.body.user});
-
-    let quantity = req.body.quantity * req.body.price
-
-    if (quantity > user.fiat) {
-        res.status(500);
-        res.send(response(false, 'quantity to sell is bigger than user´s capital'));
-    }
-
-    let token = await Token.findOne({code: req.body.token})
-
-    await Wallet.findOneAndUpdate({
-            user: user._id,
-            token: token._id
-        },
-        {$inc: {'quantity': -req.body.quantity}}
-    ).exec(async (e, d) => {
-        if (e) {
-            res.status(500)
-            res.send(response(false, 'transaction can´t be possible'));
-        } else {
-            user.fiat = user.fiat + quantity
-            await user.save()
-            res.status(200)
-            res.send(response(true, 'transaction OK'));
-        }
+    await transactionStrategy('sell', async (user, token, quantity) => {
+        await Wallet.findOneAndUpdate({
+                user: user._id,
+                token: token._id
+            },
+            {$inc: {'quantity': -req.body.quantity}}
+        ).exec(async (e, d) => {
+            if (e) {
+                res.status(500)
+                res.send(response(false, 'transaction can´t be possible'));
+            } else {
+                user.fiat = user.fiat + quantity
+                await user.save()
+                res.status(200)
+                res.send(response(true, 'transaction OK'));
+            }
+        })
     })
 })
 
 router.post('/buy', async (req, res) => {
+    await transactionStrategy('buy', async (user, token, quantity) => {
+        await Wallet.findOneAndUpdate({
+                user: user._id,
+                token: token._id
+            },
+            {$inc: {'quantity': req.body.quantity}}
+        ).exec(async (e, d) => {
+            if (e) {
+                res.status(500)
+                res.send(response(false, 'transaction can´t be possible'));
+            } else {
+                user.fiat = user.fiat - quantity
+                await user.save()
+                res.status(200)
+                res.send(response(true, 'transaction OK'));
+            }
+        })
+    }, req, res)
+})
+
+async function transactionStrategy(name, strategy, req, res) {
     if (!req.body.token || !req.body.user || !req.body.price || !req.body.quantity) {
         res.status(400);
         res.send(response(false, '"token" & "user" & "price" & "quantity" must be sent'));
@@ -129,27 +136,11 @@ router.post('/buy', async (req, res) => {
 
     if (quantity > user.fiat) {
         res.status(500);
-        res.send(response(false, 'quantity to buy is bigger than user´s capital'));
+        res.send(response(false, 'quantity to ' + name + ' is bigger than user´s capital'))
     }
 
     let token = await Token.findOne({code: req.body.token})
-
-    await Wallet.findOneAndUpdate({
-            user: user._id,
-            token: token._id
-        },
-        {$inc: {'quantity': req.body.quantity}}
-    ).exec(async (e, d) => {
-        if (e) {
-            res.status(500)
-            res.send(response(false, 'transaction can´t be possible'));
-        } else {
-            user.fiat = user.fiat - quantity
-            await user.save()
-            res.status(200)
-            res.send(response(true, 'transaction OK'));
-        }
-    })
-})
+    strategy(user, token, quantity)
+}
 
 module.exports = router;

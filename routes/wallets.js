b07072f50bd92b98c03cc8aa96ba644088d9a146
Matías Olivera <matibjolivera@gmail.com>
2021-06-16 22:21:38 -0300

    buy

--- routes/wallets.js
+++ routes/wallets.js
@@ -101,11 +101,48 @@ router.post('/sell', async (req, res) => {
             token: token._id
         },
         {$inc: {'quantity': -req.body.quantity}}
-    ).exec((e, d) => {
+    ).exec(async (e, d) => {
         if (e) {
             res.status(500)
             res.send(response(false, 'transaction can´t be possible'));
         } else {
+            user.fiat = user.fiat + req.body.quantity
+            await user.save()
+            res.status(200)
+            res.send(response(true, 'transaction OK'));
+        }
+    })
+})
+
+router.post('/buy', async (req, res) => {
+    if (!req.body.token || !req.body.user || !req.body.price || !req.body.quantity) {
+        res.status(400);
+        res.send(response(false, '"token" & "user" & "price" & "quantity" must be sent'));
+    }
+
+    let user = await User.findOne({_id: req.body.user});
+
+    let quantity = req.body.quantity * req.body.price
+
+    if (quantity > user.fiat) {
+        res.status(500);
+        res.send(response(false, 'quantity to buy is bigger than user´s capital'));
+    }
+
+    let token = await Token.findOne({code: req.body.token})
+
+    await Wallet.findOneAndUpdate({
+            user: user._id,
+            token: token._id
+        },
+        {$inc: {'quantity': req.body.quantity}}
+    ).exec(async (e, d) => {
+        if (e) {
+            res.status(500)
+            res.send(response(false, 'transaction can´t be possible'));
+        } else {
+            user.fiat = user.fiat - quantity
+            await user.save()
             res.status(200)
             res.send(response(true, 'transaction OK'));
         }

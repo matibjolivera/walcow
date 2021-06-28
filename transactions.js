72e1cdecdec51510187614adefb0b1df9893e5fe
arielbonfil <stoari33@gmail.com>
2021-06-28 01:05:48 -0300

    intento de refactor buy and sell

--- transactions.js
+++ transactions.js
@@ -1,56 +1,77 @@
-const response = require('./responses')
-const Wallet = require('./models/Wallet')
-const User = require('./models/User')
+const response = require("./responses");
+const Wallet = require("./models/Wallet");
+const User = require("./models/User");
 
 async function transaction(name, strategy, req, res) {
-    if (!req.body.token || !req.body.user || !req.body.price || !req.body.quantity) {
-        res.status(400);
-        res.send(response(false, '"token" & "user" & "price" & "quantity" must be sent'));
-    }
+  if (
+    !req.body.token ||
+    !req.body.tokenUser ||
+    !req.body.price ||
+    !req.body.quantity
+  ) {
+    res.status(400);
+    res.send(
+      response(false, '"token" & "user" & "price" & "quantity" must be sent')
+    );
+  }
 
-    let user = await User.findOne({_id: req.body.user});
+  let user = await User.findOne({ token: req.body.tokenUser });
 
-    let quantity = req.body.quantity * req.body.price
+  let quantity = req.body.quantity;
+  let amount = req.body.amount;
 
-    if (quantity > user.fiat) {
+  if (amount > user.fiat) {
+    res.status(500);
+    res.send(
+      response(false, "quantity to " + name + " is bigger than user´s capital")
+    );
+  }
+
+  strategy(user, quantity).then((r) => {
+    Wallet.findOneAndUpdate(
+      {
+        user: user._id,
+        token: req.body.token,
+      },
+      { $inc: { quantity: r.quantity } }
+    ).exec(async (e, d) => {
+      if (e) {
         res.status(500);
-        res.send(response(false, 'quantity to ' + name + ' is bigger than user´s capital'))
-    }
-
-    strategy(user, quantity).then(r => {
-        Wallet.findOneAndUpdate({
-                user: user._id,
-                token: req.body.token
-            },
-            {$inc: {'quantity': r.quantity}}
-        ).exec(async (e, d) => {
-            if (e) {
-                res.status(500)
-                res.send(response(false, 'transaction can´t be possible'));
-            } else {
-                user.fiat = r.fiat
-                await user.save()
-                res.status(200)
-                res.send(response(true, 'transaction OK'));
-            }
-        })
-    })
+        res.send(response(false, "transaction can´t be possible"));
+      } else {
+        user.fiat = r.fiat;
+        await user.save();
+        res.status(200);
+        res.send(response(true, "transaction OK"));
+      }
+    });
+  });
 }
 
 module.exports.sell = async (req, res) => {
-    await transaction('sell', async (user, quantity) => {
-        return {
-            quantity: -req.body.quantity,
-            fiat: user.fiat + quantity
-        }
-    }, req, res)
-}
+  await transaction(
+    "sell",
+    async (user, quantity) => {
+      return {
+        quantity: -req.body.quantity,
+        fiat: user.fiat + quantity,
+      };
+    },
+    req,
+    res
+  );
+};
 
 module.exports.buy = async (req, res) => {
-    await transaction('buy', async (user, quantity) => {
-        return {
-            quantity: req.body.quantity,
-            fiat: user.fiat - quantity
-        }
-    }, req, res)
-}
+  await transaction(
+    "buy",
+    async (user, quantity) => {
+      return {
+        quantity: req.body.quantity,
+        fiat: user.fiat - quantity,
+      };
+    },
+    req,
+    res
+  );
+};
